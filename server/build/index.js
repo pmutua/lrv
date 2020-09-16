c05058379850dc83d5857c0d8cf3ccd081c7688e
philip <pmutua@live.com>
2020-09-16 12:52:19 +0300

    chore:  server cors, json & urlencoded settings complete

--- server/build/index.js
+++ server/build/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 const applicationsRoutes_1 = __importDefault(require("./routes/applicationsRoutes"));
 class Server {
@@ -16,6 +17,9 @@ class Server {
     config() {
         this.app.set('port', process.env.PORT || 3000);
         this.app.use(morgan_1.default('dev'));
+        this.app.use(cors_1.default());
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
     }
     routes() {
         this.app.use('/', indexRoutes_1.default);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express, {Application} from 'express';
 import morgan from 'morgan';
-import cors from cors;
+import cors from 'cors';
 
 import indexRoutes from './routes/indexRoutes';
 import applicationsRoutes from './routes/applicationsRoutes';
@@ -18,7 +18,10 @@ class Server {
 
     config(): void{
         this.app.set('port',process.env.PORT || 3000)
-        this.app.use(morgan('dev'))
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+        this.app.use(express.json())
+        this.app.use(express.urlencoded({extended:false}))
 
     }
 
